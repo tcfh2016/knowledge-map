eaad0e1a2a02fe5118b9ba7c86da523fc502f6f3
tcfh2016 <tcfh2016@gmail.com>
2019-07-15 21:56:31 +0800

    Reading note.

---
+++ finance/books/Python_for_Finance/II_Financial_Analytics_and_Development/Chapter_9_Mathmatical_Tools/maximazation_problem_formulation_min.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import scipy.interpolate as spi
+import scipy.optimize as spo
+
+# function to be minimized
+from math import sqrt
+def Eu(*args):
+    s = args[0]
+    b = args[1]
+    return -(0.5 * sqrt(s * 15 + b * 5) + 0.5 * sqrt(s * 5 + b * 12))
+# constraints
+cons = ({'type':'ineq', 'fun':(lambda s,b:100-s*10-b*10)})
+# budget constraint
+bnds = ((0, 1000), (0, 1000))  # uppper bounds large enough
+
+result = spo.minimize(Eu, [5, 5], method='SLSQP', bounds=bnds, constraints=cons)
+print(result)
