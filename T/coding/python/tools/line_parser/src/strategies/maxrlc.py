a80680684d8f48119b5292903373769bf82878f7
tcfh2016 <tcfh2016@gmail.com>
2021-07-29 14:15:18 +0800

    update line parser script.

--- T/coding/python/tools/line_parser/src/strategies/maxrlc.py
+++ T/coding/python/tools/line_parser/src/strategies/maxrlc.py
@@ -1,31 +1,36 @@
 import src.strategies.strategy as strategy
 
+'''
+ASP-1831-Disp_3 <2021-07-27T08:22:56.794262Z> 14B-L2RlcDLQ INF/L2HiDu, g1r003u823Bucp1199t0010h04/RlcDlRbBase.cpp:120/Sending BearerErrorIndication due to MaxRlcRetransmission exceeded
+'''
 class MaxRlc(strategy.Strategy):
     def __init__(self):
         pass
 
     def construct_head_line(self):
-        head_line = "cellId," + "crnti," + "ueId," + "ueIndex," + "lcid\n"
+        head_line = "BearGroup," + "BearIndex," + "UeId," + "UeIdCu," + "LodataUeId," + "LogicalChannelId\n"
         return head_line
 
     def doParse(self, line):
-        target_start = line.find("L2TupuDl/") + len("L2TupuDl/")
-        target_end = line.find("/MAC")
+        target_start = line.find("L2HiDu,") + len("L2HiDu,")
+        target_end = line.find("/RlcDlRbBase")
         target = line[target_start:target_end]
         print (target)
 
-        cStart = target.find('c')
+        gStart = target.find('g')
         rStart = target.find('r')
         uStart = target.find('u')
-        xStart = target.find('x')
-        lStart = target.find('l')
+        pStart = target.find('ucp')
+        tStart = target.find('t')
+        hStart = target.find('h')
 
-        cellId  = int(target[cStart+1:rStart], 16)
-        crnti   = int(target[rStart+1:uStart], 16)
-        ueId    = int(target[uStart+1:xStart], 16)
-        ueIndex = int(target[xStart+1:lStart], 16)
-        lcid    = int(target[lStart+1:], 16)
+        BearGroup         = int(target[gStart+1:rStart], 16)
+        BearIndex         = int(target[rStart+1:uStart], 16)
+        UeId              = int(target[uStart+1:pStart], 16)
+        UeIdCu            = int(target[pStart+3:tStart], 16)
+        LodataUeId        = int(target[tStart+1:hStart], 16)
+        LogicalChannelId  = int(target[hStart+1:], 16)
 
-        output_line = str(cellId) + "," + str(crnti) + "," + str(ueId) + "," + str(ueIndex) + "," + str(lcid) + "\n"
+        output_line = str(BearGroup) + "," + str(BearIndex) + "," + str(UeId) + "," + str(UeIdCu) + "," + str(LodataUeId) + "," + str(LogicalChannelId) + "\n"
         print (output_line)
         return output_line
