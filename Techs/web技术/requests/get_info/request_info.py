12e54fe0199b0020d8dda83ac46b274d6830328a
tcfh2016 <tcfh2016@gmail.com>
2022-09-19 07:47:53 +0800

    update

--- Techs/web技术/requests/get_info/request_info.py
+++ Techs/web技术/requests/get_info/request_info.py
@@ -9,7 +9,7 @@ proxies = {
 }
 
 '''
-url = "https://ece-ci.dynamic.nsn-net.net/job/MASTER/job/GNB/job/UPLANE/job/L2-LO/job/SCT.fuse.asib_abio/32196/"
+url = "..."
 res = requests.get(url, proxies=proxies, verify=False)
 #with open("text.txt", "w", encoding="utf-8") as f:
 #    f.write(res.text)
@@ -25,12 +25,13 @@ print (date < today)
 '''
 requests.packages.urllib3.disable_warnings()
 
-url = "https://ece-ci.dynamic.nsn-net.net/job/MASTER/job/GNB/job/UPLANE/job/L2-LO/job/SCT.fuse.asib_abio/32196/console"
+url = "https://ece-ci.dynamic.nsn-net.net/job/MASTER/job/GNB/job/UPLANE/job/L2-LO/job/SCT.fuse.asib_abio/32553/console"
 res = requests.get(url, proxies=proxies, verify=False)
 with open("text.txt", "w", encoding="utf-8") as f:
     f.write(res.text)
 
 bs = BeautifulSoup(res.text, 'html.parser')
-s = bs.find(string=re.compile('ZUUL_CHANGE'))
+s = bs.find(string=re.compile("\'ZUUL_CHANGE\'"))
+print(s)
 gerrit_id = str(s).split(":")[1].split("'")[1]
 print(gerrit_id)
